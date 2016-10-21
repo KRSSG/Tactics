2c83f0a646c955bc725d053f2c4ba21c94b2b1a4
gsengupta2810 <gsengupta2810@gmail.com>
2016-10-21 16:30:22 +0530

    updated completed function

--- src/tAttackSupport1_Center.cpp
+++ src/tAttackSupport1_Center.cpp
@@ -30,12 +30,8 @@ namespace Strategy{
 		Vector2D<int> botPos(bs.homePos[botID].x, bs.homePos[botID].y);
 	    Vector2D<int> ballPos(bs.ballPos.x, bs.ballPos.y);
 	    float ballDist = Vector2D<int>::dist(botPos, ballPos);
-	    fstream f;
-	    f.open("/home/gunjan/catkin_ws/src/play/receive.txt",fstream::out|fstream::app);
-	    f<<"here"<<endl;
-	    if(ballDist<1.2*DRIBBLER_BALL_THRESH) f<<"completed"<<endl;
-	    f.close();
-	    if(ballDist<1.2*DRIBBLER_BALL_THRESH) return true;
+	    if(ballDist<2*DRIBBLER_BALL_THRESH) 
+	    	return true;
 	    return false;
 	}
 	bool TAttackSupport1_Center::isActiveTactic(void)const{
--- src/tMark.cpp
+++ src/tMark.cpp
@@ -50,7 +50,6 @@ namespace Strategy{
 		for(std::list<int>::const_iterator itr = freeBots.begin(); itr != freeBots.end(); ++itr){
 			for(int i = 0; i < away_bots_on_our_goalie_side.size(); ++i){
 				Vector2D<int> awayBotPos(state.awayPos[away_bots_on_our_goalie_side[i]].x,state.awayPos[away_bots_on_our_goalie_side[i]].y);
-				//Vector2D<int> homeBotPos(state.homePos[botID].x, state.homePos[botID].y);
 				Vector2D<int> homeBotPos(state.homePos[*itr].x, state.homePos[*itr].y);
 				if(Vector2D<int>::dist(awayBotPos,homeBotPos) < min_dis){
 					best_bot = *itr;
